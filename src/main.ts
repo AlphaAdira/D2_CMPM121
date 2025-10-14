b7fedb2fc0fdde577c9a2f2a12bbb75f8f3d855a
Adira Rana <58012161+AlphaAdira@users.noreply.github.com>
2025-10-14 21:45:13 +0000

    step 1 complete added a title (using h1) added a canvas thats 256x256 gave that canvas a border gave canvas rounded corners gave canvas a drop shadow

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
+  <h1>[App Title]</h1>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <canvas width="256" height="256"></canvas> 
 `;
